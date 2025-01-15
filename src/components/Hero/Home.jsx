b1d69216da989d17c991ae2910f976f5f939064d
PIAI23MAR1010 <sshermathangam@gmail.com>
2025-01-15 15:17:41 +0530

    UI Update

---
+++ src/components/Hero/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from "./Hero.module.css";
+import { getImageUrl } from '../../utils';
+
+const Home = () => {
+    return (
+        
+            <>
+            <div className={styles.contentText}>
+                <h1 className={styles.title}>Hi, I'm SHERMA THANGAM S</h1>
+                <p className={styles.description}>
+                    I am computer science student with a keen eye for detail and a robust understanding of web development technologies. My passion lies in applying my skills to practical projects. I consider myself to be a lifelong learner.
+                    <br />
+                    <br />
+                    As a quick learner, team player, and effective communicator, I bring focus and enthusiasm to every challenge. I thrive in collaborative environments and am driven by a passion for innovation and impactful solutions.
+                </p>
+                <a href="mailto:sshermathangam@gmail.com" className={styles.contactBtn}>
+                    Contact Me
+                </a>
+            </div>
+            <img
+                src={getImageUrl("hero/hero.png")}
+                alt="Hero image of me"
+                className={styles.heroImg}
+            />
+            </>
+        
+    )
+}
+
+export default Home
